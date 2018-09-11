86bdde0ae36f7a018fe5a83b2aa3fdd3b99dcb87
Candy Susan <candysusan55@gmail.com>
2018-09-11 17:40:49 +0300

    Test missing elements

--- test_missing_elements.py
+++ test_missing_elements.py
@@ -1,12 +1,11 @@
 import unittest
-from missing_elements import *
+from missing_elements import missing_numbers
 class MissingElements(unittest.TestCase):
      """
         This tests methods in missing_elements file
      """
      def test_missing_elements(self):
-         self.assertEqual((4,8),missing_numbers.missing_elements([1,2,3,5,6,7,9]),msg='The output should be a missing_element')
-
+         self.assertEqual((4,8),missing_elements.missing_elements([1,2,3,5,6,7,9]),msg='The output should be a missing_element')
 
 if __name__ == "__main__":
     unittest.main()
